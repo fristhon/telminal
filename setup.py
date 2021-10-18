79dd02142e0abd89eb7d27d77494ac428c1e5b23
Mohammadreza Jafari <fristhon97@gmail.com>
2021-10-18 20:15:38 +0330

    big bang

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="telminal",
+    version="0.1.0",
+    description="",
+    long_description="",
+    author="Mohammadreza Jafari",
+    author_email="fristhon@outlook.com",
+    license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+    packages=find_packages(),
+    include_package_data=True,
+)
