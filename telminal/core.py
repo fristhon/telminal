1339d7b2613c746cb98727e26aab137844d6d7cf
Mohammadreza Jafari <fristhon97@gmail.com>
2021-10-23 19:40:29 +0330

    add telminal class

--- telminal/core.py
+++ telminal/core.py
@@ -5,6 +5,9 @@ from time import time
 import pexpect
 from pexpect.exceptions import EOF
 from pexpect.exceptions import TIMEOUT
+from telethon import events
+
+from .telegram import Telegram
 
 
 class TProcess:
@@ -62,3 +65,72 @@ class TProcess:
     def last_message(self, last_message: str):
         self._last_message = last_message
         self.new_data = False
+
+
+class Telminal:
+    all_process = {}
+
+    def __init__(
+        self, *, api_id: int, api_hash: str, token: str, session_name: str = "telminal"
+    ) -> None:
+        self.interactive_process = None
+        self.bot = Telegram(api_id, api_hash, token, session_name)
+
+    async def start(self):
+        handlers = {
+            self.all_messages_handler: events.NewMessage(incoming=True),
+        }
+        await self.bot.start(handlers)
+
+    def new_process(self, command: str, request_id: int) -> TProcess:
+        process = TProcess(command, request_id)
+        process.run()
+        return process
+
+    async def all_messages_handler(self, event):
+        self.bot.chat_id = event.chat_id  # TODO
+        command: str = event.message.message
+        process = self.new_process(command, event.message.id)
+        # TODO better?
+        Telminal.all_process[process.pid] = process
+
+        asyncio.shield(self.run_in_background(process))
+
+    async def response(self, process: TProcess):
+        result = process.full_output
+        # handle telegram caption limit
+        if len(result) >= Telegram.MEDIA_CAPTION_LIMIT:
+            result = result[len(result) - Telegram.MEDIA_CAPTION_LIMIT :]
+
+        # there is no output for commands like `touch`
+        # telegram api raise an error for no changes edit
+        if not result or process.last_message == result:
+            return
+
+        if process.is_partial:
+            await self.bot.edit_message(result, message_id=process.response_id)
+        else:
+            process.response_id = (
+                await self.bot.send_message(result, reply_to=process.request_id)
+            ).id
+            process.is_partial = True
+        process.last_message = result
+
+    async def run_in_background(self, process: TProcess):
+        while process.is_running:
+            partial_update_time = (process.run_time + 1) % 4 == 0
+            try:
+                if partial_update_time or process.response_id is None:
+                    # first time fast response needed but next times
+                    # for a partial update must passed at least 1 second
+                    response_delay = 0.5 if process.response_id is None else 1.1
+                    await asyncio.sleep(response_delay)
+                    await self.response(process)
+            except Exception:
+                # TODO should I reaction to this?
+                pass
+            finally:
+                await asyncio.sleep(0.1)
+
+        # maybe process will be finished before next update
+        await self.response(process)
