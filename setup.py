0208664831185f5d86a1738e197bba2bd45ba6bb
Mohammadreza Jafari <fristhon97@gmail.com>
2021-11-01 20:26:10 +0330

    update setup.py

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from setuptools import setup
 from setuptools import find_packages
+from setuptools import setup
 
 setup(
     name="telminal",
     version="0.1.0",
-    description="",
+    description="A Terminal for Telegram",
     long_description="",
     author="Mohammadreza Jafari",
     author_email="fristhon@outlook.com",
@@ -15,8 +15,14 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
     ],
+    install_requires=["pexpect>=4.8.0,<5", "Telethon>=1.23.0,<2", "pyppeteer>=0.2.6<1"],
+    python_requires=">=3.7",
     packages=find_packages(),
     include_package_data=True,
+    entry_points={
+        "console_scripts": [
+            "telminal = telminal.main:main",
+        ],
+    },
 )
